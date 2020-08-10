ced0f52f265e5f7fa8804dd6782753978955d645
Alyona <alyona.rodin@gmail.com>
2020-08-10 00:08:37 -0400

    grouped bar chart

--- src/components/GroupedBarChart/Bars.js
+++ src/components/GroupedBarChart/Bars.js
@@ -11,25 +11,21 @@ export default class Bars extends Component {
       this.colorScale = d3.scaleOrdinal()
       .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
     }
- 
     render() {
       const { scales, margins, data, keys,groupKey, svgDimensions, ...props } = this.props
       const { xScale1, xScale0, yScale } = scales
       const { height } = svgDimensions
 
       const Data=data.map(d=>keys.map(key=>({key,value:d[key]})))
-      // console.log(Data)
-      
-
+      console.log(Data)
       const bars = (
         Data.map(datum =>
           datum.map(d=>
-           
             <rect
             // key={datum.state}
-            x={xScale1(d.key)}  
+            x={xScale1(d.key)} 
             y={yScale(d.value)}
-            height={scales.yScale(0) - scales.yScale(d.value)}
+            height={scales.yScale(0)-scales.yScale(d.value)}
             width={xScale1.bandwidth()}
             fill={this.colorScale(d.key)}
           />
@@ -37,13 +33,11 @@ export default class Bars extends Component {
         )
       )
       return (
-        data.map(d=>
+          data.map(d=>
           <g transform={`translate(${xScale0(d[groupKey])},0)`}>
-               {bars}
-          </g>)
-        
-        
-          
+              {bars}
+          </g>
+          )
       )
     }
 }
